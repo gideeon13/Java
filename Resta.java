8cfc8335cc1ec8c809a5f913d42addc70f82994f
Deiann <80565063+gideeon13@users.noreply.github.com>
2023-09-05 22:49:36 -0300

    Delete Resta.java

--- Resta.java
+++
@@ -1,9 +0,0 @@
-public class Resta extends Operar{
-   double resta;
-   public Resta(double n1, double n2){
-     super(n1, n2, '-');
-     this.resta = n1-n2;
-     this.setRes(this.resta);
-     
-  }
-}
