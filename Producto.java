795805634a3995f674e7f48e23ccb590b2a91105
Deiann <80565063+gideeon13@users.noreply.github.com>
2023-09-05 22:49:46 -0300

    Delete Producto.java

--- Producto.java
+++
@@ -1,9 +0,0 @@
-public class Producto extends Operar{
-   double producto;
-   public Producto(double n1, double n2){
-     super(n1, n2, '*');
-     this.producto = n1*n2;
-     this.setRes(this.producto);
-     
-  }
-}
