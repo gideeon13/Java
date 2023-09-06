835efc41ffef63217e3ae6d3c7ad727d2665bfc8
Deiann <80565063+gideeon13@users.noreply.github.com>
2023-09-05 22:49:27 -0300

    Delete Suma.java

--- Suma.java
+++
@@ -1,9 +0,0 @@
-public class Suma extends Operar{
-   double suma;
-   public Suma(double n1, double n2){
-     super(n1, n2, '+');
-     this.suma = n1+n2;
-     this.setRes(this.suma);
-     
-  }
-}
